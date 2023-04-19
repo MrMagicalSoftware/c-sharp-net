9e2786253aa55e386c4faa2314141ee2f424843f
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2023-04-19 11:58:32 +0200

    Add files via upload

---
+++ RestSharpExample/RestSharpExample/Fox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RestSharpExample
+{
+	public class Fox
+	{
+
+		[System.Text.Json.Serialization.JsonPropertyName("image")]
+        public string  aglieglie { get; set; }
+        public string  link { get; set; }
+
+
+	}
+}
+
---
+++ RestSharpExample/RestSharpExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using RestSharp;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // Creazione del client per effettuare la richiesta
+        var client = new RestClient("https://randomfox.ca/floof/");
+
+        // Creazione della richiesta
+        var request = new RestRequest("", Method.Get);
+        //request.AddUrlSegment("id", "123"); // Passaggio di un parametro nella URL
+
+        // Esecuzione della richiesta
+        var response = client.Execute(request);
+
+
+        //Deserialize
+
+
+        var foxResponse = JsonSerializer.Deserialize<RestSharpExample.Fox>(response.Content);
+
+
+        //.Serialization.Json.JsonSerializer();
+
+
+        // Controllo della risposta del server
+        if (response.IsSuccessful)
+        {
+            Console.WriteLine("Risposta del server:");
+            Console.WriteLine(response.Content);
+
+            Console.WriteLine("################");
+
+            Console.WriteLine(foxResponse.aglieglie);
+            Console.WriteLine(foxResponse.link);
+
+            Console.WriteLine("################");
+
+        }
+        else
+        {
+            Console.WriteLine("Errore nella richiesta:");
+            Console.WriteLine(response.ErrorMessage);
+        }
+    }
+}
