aa5d854b5c14f3711a943350230d5dd522724d62
MrMagicalSoftware <98833112+MrMagicalSoftware@users.noreply.github.com>
2023-05-04 14:48:24 +0200

    Add files via upload

---
+++ CoreExaxple1/CoreExaxple1/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    if(context.Request.Method == HttpMethods.Get  && context.Request.Query["custom"] == "true")
+    {
+
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("custom middleware \n");
+    }
+
+
+    await next();
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
